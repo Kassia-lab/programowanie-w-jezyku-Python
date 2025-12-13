b424244b1d0db11d5e1c0f44935d33b67fd19594
Kasia <kassia@vp.pl>
2025-12-13 16:04:02 +0100

    dostosowanie do standardu PEP8

--- zad_1/a.py
+++ zad_1/a.py
@@ -2,6 +2,7 @@ def wyswietl_imiona(lista_imion):
     for imie in lista_imion:
         print(imie)
 
+
 # Przykład:
 imiona = ["Anna", "Piotr", "Marek", "Kasia", "Tomek"]
 wyswietl_imiona(imiona)
--- zad_1/b.py
+++ zad_1/b.py
@@ -3,12 +3,18 @@ def podwoj_for(lista_liczb):
     for liczba in lista_liczb:
         nowa_lista.append(liczba * 2)
     return nowa_lista
+
+
 # Przykład:
 liczby = [1, 2, 3, 4, 5]
+
+
 print(podwoj_for(liczby))
+
 
 def podwoj_lista_skladana(lista_liczb):
     return [liczba * 2 for liczba in lista_liczb]
+
 
 # Przykład:
 liczby = [1, 2, 3, 4, 5]
